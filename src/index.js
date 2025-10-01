5768e25d36d33384825e3c53552511808c23c4d0
nimurr <nimurnerob404@gmail.com>
2025-10-01 09:59:04 +0600

    `Implement cluster mode with multiple worker processes and socket.io support`

--- src/index.js
+++ src/index.js
@@ -1,58 +1,68 @@
+const cluster = require("cluster");
+const os = require("os");
 const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./config/config");
 const logger = require("./config/logger");
 
-// My Local IP Address
 const myIp = process.env.BACKEND_IP;
+const numCPUs = os.cpus().length;
 
-let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info("Connected to MongoDB");
-  server = app.listen(config.port, myIp, () => {
-    // logger.info(`Listening to port ${config.port}`);
-    logger.info(`Listening to ip http://${myIp}:${config.port}`);
+// ✅ MASTER PROCESS
+if (cluster.isMaster) {
+  logger.info(`Master ${process.pid} is running with ${numCPUs} workers`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    logger.info(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
   });
 
-  //initializing socket io
-  // const socketIo = require("socket.io");
-  // const socketIO = require("./utils/socketIO");
-  // const io = socketIo(server, {
-  //   cors: {
-  //     origin: "*"
-  //   },
-  // });
+// ✅ WORKER PROCESSES
+} else {
+  mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+    logger.info(`Worker ${process.pid} connected to MongoDB`);
 
-  // socketIO(io);
-
-  // global.io = io;
-  server.listen(config.port, process.env.BACKEND_IP, () => {
-    // logger.info(`Socket IO listening to port ${config.port}`);
-  });
-});
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info("Server closed");
-      process.exit(1);
+    const server = app.listen(config.port, myIp, () => {
+      logger.info(`Worker ${process.pid} running at http://${myIp}:${config.port}`);
     });
-  } else {
+
+    // ✅ OPTIONAL: SOCKET.IO SUPPORT
+    // const socketIo = require("socket.io");
+    // const socketIO = require("./utils/socketIO");
+    // const io = socketIo(server, {
+    //   cors: {
+    //     origin: "*",
+    //   },
+    // });
+    // socketIO(io);
+    // global.io = io;
+
+    const exitHandler = () => {
+      server.close(() => {
+        logger.info(`Worker ${process.pid} closed`);
+        process.exit(1);
+      });
+    };
+
+    const unexpectedErrorHandler = (error) => {
+      logger.error(error);
+      exitHandler();
+    };
+
+    process.on("uncaughtException", unexpectedErrorHandler);
+    process.on("unhandledRejection", unexpectedErrorHandler);
+
+    process.on("SIGTERM", () => {
+      logger.info(`Worker ${process.pid} received SIGTERM`);
+      server.close();
+    });
+
+  }).catch((err) => {
+    logger.error(`MongoDB connection failed in worker ${process.pid}: ${err}`);
     process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-
-process.on("SIGTERM", () => {
-  logger.info("SIGTERM received");
-  if (server) {
-    server.close();
-  }
-});
+  });
+}
